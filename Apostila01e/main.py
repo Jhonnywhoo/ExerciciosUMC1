c2fee17fa8755af02a9cd23aa1281a65608294a3
Jhonnywhoo <jhonnywobrito@gmail.com>
2024-03-15 14:49:03 -0300

    origin

--- Apostila01e/main.py
+++ Apostila01e/main.py
@@ -4,29 +4,38 @@ os.system('cls')
 x = 0
 while x < 4:
     p1 = float(input("Entre com a primeira nota: "))
-    x += 1
-    if p1 > 10:
+    while p1 > 10:
         print("A nota não pode ser maior que 10.")
-        break
+        p1 = float(input("Entre com a primeira nota: "))
+        x += 1
+    x += 1
+    
     p2 = float(input("Entre com a segunda nota: "))
-    x += 1
-    if p2 > 10:
+    while p2 > 10:
         print("A nota não pode ser maior que 10.")
-        break
+        p2 = float(input("Entre com a segunda nota: "))
+        x += 10
+    x += 1
+    
     tp = float(input("Entre com a nota de trabalho: "))
     x += 1
-    if tp > 10:
+    while tp > 10:
         print("A nota não pode ser maior que 10.")
-        break
-    iel = float(input("Entre com a nota de laboratório: "))
+        tp = float(input("Entre com a nota de trabalho: "))
+        x += 1
     x += 1
-    if iel > 1:
+    
+    iel = float(input("Entre com a nota de laboratório: "))
+    while iel > 1:
         print("A nota DE LABORATÓRIO não pode ser maior que 1.")
-        break
+        iel = float(input("Entre com a nota de laboratório: "))
+        x += 1
+    x += 1
+
 else:
     np = (p1+p2+tp)*0.3 + (iel)*0.1
     print("A nota parcial desse aluno é: {:.2f}" .format(np))
-    if np < 7 : {
+    if np < 6 : {
         print("REPROVADO")
     }
     else : {
